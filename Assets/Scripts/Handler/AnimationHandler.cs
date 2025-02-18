4c0628d594ed3605366d5cd9787f97519587296a
thdwpdn26 <songjeu26@naver.com>
2025-02-18 14:35:15 +0900

    [Feat]플레이어의 이동 구현 완료

---
+++ Assets/Scripts/Handler/AnimationHandler.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationHandler : MonoBehaviour
+{
+    private static readonly int IsMove = Animator.StringToHash("IsMove");
+    private static int MoveX = Animator.StringToHash("MoveX");
+    private static int MoveY = Animator.StringToHash("MoveY");
+
+    private Animator animator;
+
+    private float tempX = 0f;
+    private float tempY = 0f;
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    
+    //매개변수의 값이 0.5 이상인지 확인하고 IsMove의 상태값을 바꾸고, 바라보는 방향을 유지하는 메서드
+    public void AnimatorMove(Vector2 value)
+    {
+        bool isMoving = value.magnitude > 0.5f;
+
+        animator.SetBool(IsMove, isMoving);
+
+        if(isMoving)
+        {
+            animator.SetFloat(MoveX, value.x);
+            animator.SetFloat(MoveY, value.y);
+
+            tempX = value.x;
+            tempY = value.y;
+        }
+        else
+        {
+            animator.SetFloat(MoveX, tempX);
+            animator.SetFloat(MoveY, tempY);
+        }
+    }
+}
