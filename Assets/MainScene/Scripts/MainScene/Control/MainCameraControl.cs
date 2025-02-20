2400fa4332b4ece0c4a223f67b974db5917d09bb
thdwpdn26 <songjeu26@naver.com>
2025-02-20 15:34:22 +0900

    [Docs] 메서드 주석 추가

--- Assets/MainScene/Scripts/MainScene/Control/MainCameraControl.cs
+++ Assets/MainScene/Scripts/MainScene/Control/MainCameraControl.cs
@@ -13,6 +13,13 @@ namespace MainScene
 
         private void Update()
         {
+            CameraMovement();
+        }
+
+        
+        //카메라의 위치를 조정하는 메서드
+        private void CameraMovement()
+        {
             float cameraY = Mathf.Clamp(player.transform.position.y, cameraMinY, cameraMaxY);
             Vector3 targetPosition = new Vector3(0, cameraY, -10);
             transform.position = Vector3.Lerp(transform.position, targetPosition, cameraSpeed * Time.deltaTime);
